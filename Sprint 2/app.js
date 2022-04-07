b53bf8e0d24dec06ceb9c7bc860a1943f82ea29a
Christian-Leonel <christiandiazluna@gmail.com>
2022-04-07 17:17:57 -0300

    "Orden app.js"

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -8,9 +8,20 @@ app.listen(3000, ()=>{
     console.log('Server Status: OK');
 });
 
-app.get('/', (req,res)=>{res.sendFile(__dirname + '/views/index.html');});
-app.get("/home", (req,res) => {res.sendFile(path.join(__dirname, "./views/index.html"));});
-app.get("/product-detail", (req,res) => {res.sendFile(path.join(__dirname, "./views/productDetail.html"));});
-app.get("/product-cart", (req,res) => {res.sendFile(path.join(__dirname, "./views/productCart.html"));});
-app.get("/register", (req,res) => {res.sendFile(path.join(__dirname, "./views/register.html"));});
-app.get("/login", (req,res) => {res.sendFile(path.join(__dirname, "./views/login.html"));});
+app.get('/', (req,res)=>
+    {res.sendFile(__dirname + '/views/index.html');});
+
+app.get("/home", (req,res) => 
+    {res.sendFile(path.join(__dirname, "./views/index.html"));});
+
+app.get("/product-detail", (req,res) => 
+    {res.sendFile(path.join(__dirname, "./views/productDetail.html"));});
+
+app.get("/product-cart", (req,res) => 
+    {res.sendFile(path.join(__dirname, "./views/productCart.html"));});
+
+app.get("/register", (req,res) => 
+    {res.sendFile(path.join(__dirname, "./views/register.html"));});
+
+app.get("/login", (req,res) => 
+    {res.sendFile(path.join(__dirname, "./views/login.html"));});
