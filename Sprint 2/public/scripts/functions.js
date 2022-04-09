3f2c0cef05f64b1cd478a99a650071852af2d942
Marcos Ramos <marcosramos.dm@gmail.com>
2022-04-09 16:29:37 -0300

    Home: carousel, responsive sitio en general, imagenes, etc

---
+++ Sprint 2/public/scripts/functions.js
@@ -0,0 +1,96 @@
+feather.replace();
+
+function toogleMenuMobile() {
+   var element = document.getElementById("menu-mobile");
+   element.classList.toggle("active");
+}
+
+$(document).ready(function(){
+    $('#carousel-home').owlCarousel({
+        loop:true,
+        margin:10,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            }
+        }
+    });
+    $('#carousel-products-a').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            570:{
+                items:2
+            },
+            820:{
+                items:3
+            },
+            1024:{
+                items:4
+            }
+        }
+    });
+    $('#carousel-products-b').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        merge:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            570:{
+                items:2
+            },
+            820:{
+                items:3
+            },
+            1024:{
+                items:4
+            }
+        }
+    });
+    $('#carousel-products-c').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            570:{
+                items:2
+            },
+            820:{
+                items:3
+            },
+            1024:{
+                items:4
+            }
+        }
+    });
+    $('#carousel-products-d').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            570:{
+                items:2
+            },
+            820:{
+                items:3
+            },
+            1024:{
+                items:4
+            }
+        }
+    });
+});
